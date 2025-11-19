66be566d462d66280928e0f9b9f01fe017a63cb9
nikotheflow <bboyfoxxx@yandex.ru>
2025-11-19 22:18:37 +0300

    feat: added sale filter

--- src/modules/filter.js
+++ src/modules/filter.js
@@ -1,25 +1,33 @@
 import getData from "./getData";
-import {priceFilter} from "./filters";
+import {priceFilter, saleFilter} from "./filters";
 import renderGoods from "./renderGoods";
 
 const filter = () => {
     const minPriceInput = document.getElementById("min");
     const maxPriceInput = document.getElementById("max");
+    const discountCheckbox = document.getElementById("discount-checkbox");
+    const discountCheckboxCheckmark = document.querySelector(".filter-check_checkmark");
 
-    minPriceInput.addEventListener("input", async () =>
-        await updateGoods(getCurrentMinPrice(), genCurrentMaxPrice()));
+    minPriceInput.addEventListener("input", async () => await updateGoods());
+    maxPriceInput.addEventListener("input", async () => await updateGoods());
 
-    maxPriceInput.addEventListener("input", async () =>
-        await updateGoods(getCurrentMinPrice(), genCurrentMaxPrice()));
+    discountCheckbox.addEventListener("change", async () => {
+        discountCheckboxCheckmark.classList.toggle("checked", getCurrentIsOnSale());
+
+        await updateGoods();
+    });
+
+
+    const updateGoods = async () => {
+        const goods = await getData();
+        const saleFilteredGoods = saleFilter(goods, getCurrentIsOnSale());
+        const priceFilteredGoods = priceFilter(saleFilteredGoods, getCurrentMinPrice(), getCurrentMaxPrice());
+        await renderGoods(priceFilteredGoods);
+    };
 
     const getCurrentMinPrice = () => minPriceInput.value === '' ? 0 : Number(minPriceInput.value);
-    const genCurrentMaxPrice = () => maxPriceInput.value === '' ? Infinity : Number(maxPriceInput.value);
-
-    const updateGoods = async (min, max) => {
-        const goods = await getData();
-        const filteredGoods = priceFilter(goods, min, max);
-        await renderGoods(filteredGoods);
-    };
+    const getCurrentMaxPrice = () => maxPriceInput.value === '' ? Infinity : Number(maxPriceInput.value);
+    const getCurrentIsOnSale = () => discountCheckbox.checked;
 };
 
 export default filter;
--- src/modules/filters.js
+++ src/modules/filters.js
@@ -1,11 +1,13 @@
-export const searchFilter = (goods, value) => goods
-    .filter(goodsItem => goodsItem.title.toLowerCase().includes(value.toLowerCase())
+export const searchFilter = (goods, value) =>
+    goods.filter(goodsItem => goodsItem.title.toLowerCase().includes(value.toLowerCase())
 );
 
-export const categoryFilter = (goods, category) => goods
-    .filter(goodsItem => goodsItem.category.toLowerCase().includes(category.toLowerCase())
+export const categoryFilter = (goods, category) =>
+    goods.filter(goodsItem => goodsItem.category.toLowerCase().includes(category.toLowerCase())
 );
 
-export const priceFilter = (goods, min = 0, max = Infinity) => {
-    return goods.filter((goodsItem) => min <= Number(goodsItem.price) && Number(goodsItem.price) <= max);
-};
+export const priceFilter = (goods, min = 0, max = Infinity) =>
+    goods.filter((goodsItem) => min <= Number(goodsItem.price) && Number(goodsItem.price) <= max);
+
+export const saleFilter = (goods, isSaleOnly = false) =>
+    isSaleOnly ? goods.filter((goodsItem) => goodsItem.sale) : goods;
